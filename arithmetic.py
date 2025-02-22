eacf38a3400d1e0700c76b0ebd0d13b441ce003b
thavamani18 <thavamani.tamtree@gmail.com>
2025-02-22 22:10:40 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,18 @@
+############  arithmetic oprators #################
+
+number1=72
+number2=56
+sum=number1+number2
+print("sum=",sum)
+sub=number1-number2
+print("subtraction=",sub)
+mul=number1*number2
+print("multiplication=",mul)
+div=number1/number2
+print("division=",div)
+mod=number1%number2
+print("modulus=",mod)
+ex=number1**number2
+print("exponent=",ex)
+fd=number1//number2
+print("floor division=",fd)
