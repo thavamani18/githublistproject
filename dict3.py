aa8321ce584c8674464733b0f45992f4997cb53a
thavamani18 <thavamani.tamtree@gmail.com>
2025-02-22 22:15:30 +0530

    Add files via upload

---
+++ dict3.py
@@ -0,0 +1,7 @@
+############# dictionary methods #############
+flower={"red":"rose","yellow":"sunflower","white":"jasmine","pink":"lily"}
+print(flower)
+for key in flower:
+    print(key)
+    
+        
---
+++ tup2.py
@@ -0,0 +1,5 @@
+########  add an item in a tuple#######
+item=("3","dog","deer","cat",34.5,"e")
+f=("34",)
+item += f
+print(item)
